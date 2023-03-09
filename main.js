1a989a6eea98008bf7439c547fe315fbeeadce93
CJE-cpu <chlwjddms7309@gmail.com>
2023-03-09 20:40:03 +0900

    add javascript

---
+++ main.js
@@ -0,0 +1,11 @@
+var bannerImg = document.getElementById("banner_img");
+var bannerImgBx = document.getElementById("banner_imgBx");
+var bannerText = document.getElementById("banner_text");
+bannerImg.addEventListener("click", imageZoomIn);
+
+function imageZoomIn() {
+    bannerImg.style.width = "500px";
+    bannerImg.style.alignSelf = "center";
+    bannerImgBx.style.width = "100%";
+    bannerText.style.display = "none";
+}
