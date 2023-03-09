5e2457769f27ad904001521b53f2dbc806b6c399
CJE-cpu <chlwjddms7309@gmail.com>
2023-03-09 21:20:03 +0900

    000

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var bannerImg = document.getElementById("banner_img");
 var bannerImgBx = document.getElementById("banner_imgBx");
 var bannerText = document.getElementById("banner_text");
+var i = "1";
 bannerImg.addEventListener("click", imageZoomIn);
 
 function imageZoomIn() {
