b3986f8571819acb1eceebece64a7d4084b4c8be
CJE-cpu <chlwjddms7309@gmail.com>
2023-03-10 19:59:17 +0900

    2023-03-10

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-var bannerImg = document.getElementById("banner_img");
-var bannerImgBx = document.getElementById("banner_imgBx");
-var bannerText = document.getElementById("banner_text");
-var i = "1";
-bannerImg.addEventListener("click", imageZoomIn);
+let bannerImg = document.getElementById("banner_img");
+let bannerImgBx = document.getElementById("banner_imgBx");
+let bannerText = document.getElementById("banner_text");
+
+bannerImg.onclick = imageZoomIn;
 
 function imageZoomIn() {
     bannerImg.style.width = "500px";
-    bannerImg.style.alignSelf = "center";
     bannerImgBx.style.width = "100%";
     bannerText.style.display = "none";
+    bannerImg.onclick = imageZoomOut;
 }
 
+function imageZoomOut() {
+    bannerImg.style.width = "320px";
+    bannerImgBx.style.width = "50%";
+    bannerText.style.display = "flex";
+    bannerImg.onclick = imageZoomIn;
+}
